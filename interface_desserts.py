7a77174c1fc2b158d0b3d0db39a8c981fe9b5288
unknown <pensee.buisson@crie.i-univ-tlse2.fr>
2025-04-29 11:07:52 +0200

    page dessert faite

--- interface_desserts.py
+++ interface_desserts.py
@@ -2,12 +2,12 @@ from tkinter import *
 from tkinter import ttk
 from categorie_repas import CategorieRepas
 
-def creer_page_dessert(ma_fenetre, connection):
-    dessert_frame = Frame(ma_fenetre, bg="#f3e0ec")
-    dessert_frame.grid(row=1, column=0, sticky="nsew")
+def creer_page_desserts(ma_fenetre, connection):
+    desserts_frame = Frame(ma_fenetre, bg="#f3e0ec")
+    desserts_frame.grid(row=1, column=0, sticky="nsew")
 
     # Déclaration de l'objet CategorieRepas
-    dessert = CategorieRepas("desserts", connection)
+    desserts = CategorieRepas("desserts", connection)
 
     # Variables liées aux champs de saisie
     reponse_nom_dessert = StringVar()
@@ -38,9 +38,9 @@ def creer_page_dessert(ma_fenetre, connection):
     # Interface
     Label(desserts_frame, text="Entrées", bg="#f3e0ec", fg="#450920", font=("Arial", 28)).grid(row=0, column=0, columnspan=2, pady=10)
 
-    Label(desserts_frame, text="Ajouter une entrée", bg="#f3e0ec", fg="#450920", font=("Arial", 18)).grid(row=1, column=0, columnspan=2, pady=5)
+    Label(desserts_frame, text="Ajouter un Dessert", bg="#f3e0ec", fg="#450920", font=("Arial", 18)).grid(row=1, column=0, columnspan=2, pady=5)
 
-    Label(desserts_frame, text="Nom du plat", bg="#f3e0ec", fg="#450920", font=("Arial", 14)).grid(row=2, column=0, padx=5, pady=5)
+    Label(desserts_frame, text="Nom du dessert", bg="#f3e0ec", fg="#450920", font=("Arial", 14)).grid(row=2, column=0, padx=5, pady=5)
     Entry(desserts_frame, textvariable=reponse_nom_dessert, width=30).grid(row=2, column=1, padx=5, pady=5)
 
     Label(desserts_frame, text="Nom de l'étudiant", bg="#f3e0ec", fg="#450920", font=("Arial", 14)).grid(row=3, column=0, padx=5, pady=5)
@@ -49,7 +49,7 @@ def creer_page_dessert(ma_fenetre, connection):
     Label(desserts_frame, text="Quantité apportée", bg="#f3e0ec", fg="#450920", font=("Arial", 14)).grid(row=4, column=0, padx=5, pady=5)
     Entry(desserts_frame, textvariable=reponse_qt_dessert_apportee, width=30).grid(row=4, column=1, padx=5, pady=5)
 
-    Button(desserts_frame, text="Ajouter une entrée", command=ajouter_dessert, bg="#a53860", fg="white", font=("Arial", 14)).grid(row=5, column=0, columnspan=2, pady=10)
+    Button(desserts_frame, text="Ajouter un dessert", command=ajouter_dessert, bg="#a53860", fg="white", font=("Arial", 14)).grid(row=5, column=0, columnspan=2, pady=10)
 
     tableau_desserts = ttk.Treeview(desserts_frame, columns=("nom", "nom_etudiant", "qt_apportee"), show="headings", height=10)
     tableau_desserts.heading("nom", text="Nom du plat")
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from database import connecter_bd, creer_tables
 from categorie_repas import CategorieRepas
 from interface_entrees import creer_page_entrees
 from interface_plats import creer_page_plats
+from interface_desserts import creer_page_desserts
 
 # Connexion à la base de données
 connexion = connecter_bd()
@@ -26,8 +27,13 @@ def afficher_page(page):
 # Création de la page Entrées à l'aide de la fonction creer_page_entrees
 entrees_frame = creer_page_entrees(ma_fenetre, connexion)
 plats_frame = creer_page_plats(ma_fenetre, connexion)
+
+# Création de la page Entrées à l'aide de la fonction creer_page_desserts
+desserts_frame = creer_page_entrees(ma_fenetre, connexion)
+plats_frame = creer_page_plats(ma_fenetre, connexion)
+
 # Liste contenant toutes les pages de l'application
-toutes_les_pages = [entrees_frame, plats_frame]
+toutes_les_pages = [entrees_frame, plats_frame, desserts_frame]
 
 # Création de la barre de navigation
 navbar = Frame(ma_fenetre, bg="#0d1b1e", height=50)
@@ -38,7 +44,7 @@ boutons = [
     ("Accueil", lambda: print("À venir")),
     ("Entrées", lambda: afficher_page(entrees_frame)),
     ("Plats", lambda: afficher_page(plats_frame)),
-    ("Desserts", lambda: print("À venir")),
+    ("Desserts", lambda: afficher_page(desserts_frame)),
     ("Boissons", lambda: print("À venir")),
     ("Paramètres", lambda: print("Paramètres"))
 ]
