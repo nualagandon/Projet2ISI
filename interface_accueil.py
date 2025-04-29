fca7931a38c8a1913e3e11d04ebd4b00ed8b0978
unknown <pensee.buisson@crie.i-univ-tlse2.fr>
2025-04-29 12:35:45 +0200

    partie entrée fini, prête à être testée

--- interface_accueil.py
+++ interface_accueil.py
@@ -6,6 +6,12 @@ def creer_page_accueil(ma_fenetre, connexion) :
     accueil_frame = Frame(ma_fenetre, bg="#f3e0ec")
     accueil_frame.grid(row=1, column=0, sticky="nsew")
 
+    #definition de la fonction permettant de savoir quel texte s'affiche en dessous de la barre de progression
+    def reste_repas(nb, nb_max) :
+        if nb_max - nb == 0 : 
+            return "Il y en a assez."
+        else : 
+            return "Il en manque " + (nb_max-nb) + ". "
     
 
     Label(accueil_frame, text="Total", bg="#f3e0ec", fg="#450920", font=("Arial", 28)).grid(row=0, column=0, columnspan=2, pady=10)
@@ -19,13 +25,14 @@ def creer_page_accueil(ma_fenetre, connexion) :
     
     #On récupère le nombre de plats enregistré dans la base
     curs = connexion.cursor()
-    requete = f"count(*) from Entrees;"
-    nb = curs.execute(requete)
+    requete = "count(*) from Entrees;"
+    nb_entrees = curs.execute(requete)
     curs.close()
     #nommer dans interface parametre, le maximum du nombre d'entree dans chaque etape du repas est nb_max_etape
-    barre_entree = ttk.Progressbar(entree_accueil_frame, length=taille).pack(pady=(taille - ((nb_max_entrees - nb)/100 * taille)))
-    
-    
+    barre_entree = ttk.Progressbar(entree_accueil_frame, length=taille).pack(pady=(taille - ((nb_max_entrees - nb_entrees)/100 * taille)))
+    barre_entree.place(x=30, y = 30)
+
+    Label(entree_accueil_frame, text=reste_repas(nb_entrees, nb_max_entrees), bg="#f3e0ec", fg="#450920", font=("Arial", 18)).grid(row=3, column=0, columnspan=2, pady=5)
 
 
 
